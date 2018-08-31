0fdf17f150e65b272117df5b09104670e73552b7
Chris Marsh <chrismarsh.c2@gmail.com>
2018-08-31 14:04:48 -0600

    fix mixed spaces and tabs for py3

--- isolate_citations.py
+++ isolate_citations.py
@@ -3,38 +3,41 @@ from panflute import *
 from functools import partial
 import sys
 
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-#converts the prefix or suffix to a plain string
+# converts the prefix or suffix to a plain string
 def to_string(elem):
-	prefix = []
-	for s in elem:
-		if hasattr(s, 'text'):
-			prefix.append(s.text)
+    prefix = []
+    for s in elem:
+        if hasattr(s, 'text'):
+            prefix.append(s.text)
 
-	prefix = ' '.join(prefix)
-	return prefix
+    prefix = ' '.join(prefix)
+    return prefix
 
-def isolate_citation(elem,doc):
 
-	# Surrounds the citations with Emph section so we can more easily find it later.
-	if isinstance(elem,Cite):
+def isolate_citation(elem, doc):
+    # Surrounds the citations with Emph section so we can more easily find it later.
+    if isinstance(elem, Cite):
 
-		for c in elem.citations:
-			prefix = to_string(c.prefix)
-			suffix = to_string(c.suffix)
-			citeType = c.mode
-			with open('citations.txt','a') as f:
-				f.write( '%s;%s;%s;%s\n' % (c.id,prefix,suffix,citeType) )
+        for c in elem.citations:
+            prefix = to_string(c.prefix)
+            suffix = to_string(c.suffix)
+            citeType = c.mode
+            with open('citations.txt', 'a') as f:
+                f.write('%s;%s;%s;%s\n' % (c.id, prefix, suffix, citeType))
 
-		return Emph(elem)
+        return Emph(elem)
+
 
 def main(doc=None):
-	with open('citations.txt','w') as f:
-		pass
-	return run_filter(isolate_citation, doc=doc)
+    with open('citations.txt', 'w') as f:
+        pass
+    return run_filter(isolate_citation, doc=doc)
+
+
 if __name__ == "__main__":
-	main()
-	
+    main()
